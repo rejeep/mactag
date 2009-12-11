ee840d3691bf19b167e561a8644e84c4aa7b5be9
Johan Andersson <johan.rejeep@gmail.com>
2009-12-11 14:08:04 +0000

    Added empty class in rails gem.

---
+++ lib/mactag/tag/rails/gem.rb
@@ -0,0 +1,15 @@
+module Mactag
+  module Tag
+    module Rails
+      class Gem
+
+        include Mactag::Tag::Rails
+
+        def files
+          []
+        end
+
+      end
+    end
+  end
+end
