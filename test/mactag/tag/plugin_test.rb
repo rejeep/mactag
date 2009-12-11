eb0a4212e98ac716177836d567e276e9e3e36ff2
Johan Andersson <johan.rejeep@gmail.com>
2009-12-11 18:27:27 +0000

    Added test for plugin.

---
+++ test/mactag/tag/plugin_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class PluginTest < ActiveSupport::TestCase
+  
+  should "have correct plugin path" do
+    assert_equal "vendor/plugins", Mactag::Tag::Plugin::PLUGIN_PATH
+  end
+  
+  context "without arguments" do
+    setup do
+      @plugin = Mactag::Tag::Plugin.new
+    end
+
+    should "return all plugins as path" do
+      assert_equal @plugin.files, "vendor/plugins/*/**/*.rb"
+    end
+  end
+  
+  context "with one plugin argument" do
+    setup do
+      @plugin = Mactag::Tag::Plugin.new("thinking-sphinx")
+    end
+    
+    should "return the path to that plugin" do
+      assert_equal ["vendor/plugins/thinking-sphinx/**/*.rb"], @plugin.files
+    end
+  end
+  
+  context "with more thatn one plugin argument" do
+    setup do
+      @plugin = Mactag::Tag::Plugin.new("thinking-sphinx", "formtastic")
+    end
+    
+    should "return the paths to those plugins" do
+      assert_contains @plugin.files, "vendor/plugins/thinking-sphinx/**/*.rb"
+      assert_contains @plugin.files, "vendor/plugins/formtastic/**/*.rb"
+    end
+  end
+  
+end
