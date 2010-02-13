bb670b94cc0188c1bad9b758e2580467a828c21f
Johan Andersson <johan.rejeep@gmail.com>
2010-02-13 16:40:29 +0000

    Load stuff in test helpers in other order.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
-$:.unshift File.dirname(__FILE__) + '/../lib'
-
 require 'rubygems'
 require 'active_support'
 require 'active_support/test_case'
 require 'active_support/core_ext'
-require 'mactag'
 require 'shoulda'
 require 'mocha'
+
+$:.unshift File.dirname(__FILE__) + '/../lib'
+require 'mactag'
