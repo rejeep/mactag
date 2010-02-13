0c27af096679163fb0d51294adc8e7e4a0bad419
Johan Andersson <johan.rejeep@gmail.com>
2010-02-13 14:55:53 +0000

    Make standard gem home the one on MacOS.

--- lib/mactag/config.rb
+++ lib/mactag/config.rb
@@ -7,13 +7,13 @@ module Mactag
   #
   # ==== Gem Home
   # The folder where the gems are stored.
-  #   Mactag::Config.gem_home = "/usr/lib/ruby/gems/1.9/gems"
+  #   Mactag::Config.gem_home = "/Library/Ruby/Gems/1.8/gems"
   class Config
 
     @@binary = "ctags -o TAGS -e"
     cattr_accessor :binary
     
-    @@gem_home = "/usr/lib/ruby/gems/1.8/gems"
+    @@gem_home = "/Library/Ruby/Gems/1.8/gems"
     cattr_accessor :gem_home
 
   end
