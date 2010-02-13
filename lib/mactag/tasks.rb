b8127c382cacc6144fdf0b6637a0cb2558f71550
Johan Andersson <johan.rejeep@gmail.com>
2010-02-13 15:36:33 +0000

    Removed old tasks.rb file.

--- lib/mactag/tasks.rb
+++
@@ -1,8 +0,0 @@
-task :environment
-
-desc "Creates a Ctags file"
-task :mactag => :environment do
-  require File.join(Rails.root, "config", "mactag")
-
-  system "cd #{Rails.root} && #{Mactag::Config.binary} #{Mactag::Table.tags}"
-end
