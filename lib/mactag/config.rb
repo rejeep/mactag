7fe43aba0f82c44fb7a9e964c5547d00c35191b1
Johan Andersson <johan.rejeep@gmail.com>
2010-02-13 15:20:24 +0000

    The configuration comments should be at resp. option.

--- lib/mactag/config.rb
+++ lib/mactag/config.rb
@@ -1,18 +1,13 @@
 module Mactag
-  # Configuration options.
-  #
-  # ==== Binary
-  # The command to run when creating the TAGS-file.
-  #   Mactag::Config.binary = "etags -o TAGS"
-  #
-  # ==== Gem Home
-  # The folder where the gems are stored.
-  #   Mactag::Config.gem_home = "/Library/Ruby/Gems/1.8/gems"
   class Config
 
+    # The command to run when creating the TAGS-file.
+    #   Mactag::Config.binary = "etags -o TAGS"
     @@binary = "ctags -o TAGS -e"
     cattr_accessor :binary
-    
+
+    # The folder where the gems are stored.
+    #   Mactag::Config.gem_home = "/Library/Ruby/Gems/1.8/gems"
     @@gem_home = "/Library/Ruby/Gems/1.8/gems"
     cattr_accessor :gem_home
 
