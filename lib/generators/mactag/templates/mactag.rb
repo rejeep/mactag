fe458dc43b8f0ea59d205e5206c03162e69ff5f1
Johan Andersson <johan.rejeep@gmail.com>
2010-02-13 15:12:38 +0000

    Make a better configuration file (created by the mactag generator) with more examples.

--- lib/generators/mactag/templates/mactag.rb
+++ lib/generators/mactag/templates/mactag.rb
@@ -1,11 +1,33 @@
+# Default gem home is the standard one on Mac OS (/Library/Ruby/Gems/1.8/gems).
+# Change to whatever your system is using, if not the default.
+#
+# Most GNU/Linux systems:
+#   Mactag::Config.gem_home = "/usr/lib/ruby/gems/1.8/gems"
+
+# Change the binary option if you are not satisfied with the standard
+# command (ctags -o TAGS -e) used to create the TAGS table.
+#   Mactag::Config.binary = "etags -o TAGS"
+
+# Example configuration. Change according to your application.
 Mactag::Table.generate do
-  app "app/**/*.rb", "lib/*.rb"
+  # Index all ruby files in app recursive and all ruby files directly under lib.
+  #   app "app/**/*.rb", "lib/*.rb"
 
-  plugins "thinking-sphinx", "formtastic"
+  # Index the plugins thinking-sphinx and formtastic.
+  #  plugins "thinking-sphinx", "formtastic"
 
-  gems "paperclip", "authlogic"
-  gem "formtastic", :version => "0.9.7"
+  # Index the gems paperclip and authlogic.
+  #   gems "paperclip", "authlogic"
+  
+  # Index the gem formtastic version 0.9.7.
+  #   gem "formtastic", :version => "0.9.7"
 
-  rails :except => :actionmailer
-  rails :only => [:activerecord, :active_support]
+  # Index all rails packages, except actionmailer.
+  #   rails :except => :actionmailer
+  
+  # Index only rails packages activerecord and activesupport.
+  #   rails :only => [:activerecord, :active_support]
+  
+  # Index all rails packages, version 2.3.5.
+  #   rails :version => "2.3.5"
 end
