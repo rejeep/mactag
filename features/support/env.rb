5e146e9efcdfa06b70747613a3dca862176ec16b
Johan Andersson <johan.rejeep@gmail.com>
2010-03-11 19:25:37 +0000

    Make tests run.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'active_support'
 require 'active_support/test_case'
+require 'active_support/core_ext'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'active_support'
 require 'active_support/test_case'
+require 'active_support/core_ext'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
+
 require 'mactag'
 require 'shoulda'
 require 'mocha'
