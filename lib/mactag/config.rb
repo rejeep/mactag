c97b87ad4f6db7a461e2fe181add249fd576a8e5
Johan Andersson <johan.rejeep@gmail.com>
2010-07-31 12:22:45 +0200

    Fixed some comment styles.

--- lib/mactag/config.rb
+++ lib/mactag/config.rb
@@ -1,15 +1,19 @@
 module Mactag
   class Config
-
+    ##
+    #
     # The command to run when creating the TAGS-file.
     #   Mactag::Config.binary = "etags -o TAGS"
+    #
     @@binary = "ctags -o TAGS -e"
     cattr_accessor :binary
 
-    # The folder where the gems are stored.
+    ##
+    #
+    # The system folder where the gems are located.
     #   Mactag::Config.gem_home = "/Library/Ruby/Gems/1.8/gems"
+    #
     @@gem_home = "/Library/Ruby/Gems/1.8/gems"
     cattr_accessor :gem_home
-
   end
 end
--- lib/mactag/tag/parser.rb
+++ lib/mactag/tag/parser.rb
@@ -1,33 +1,43 @@
 module Mactag
   module Tag
     class Parser
-
       def initialize(table)
         @table = table
       end
 
+      ##
+      #
       # @see Mactag::Tag::App
+      #
       def app(*files)
         @table << Mactag::Tag::App.new(*files)
       end
 
+      ##
+      #
       # @see Mactag::Tag::Plugin
+      #
       def plugin(*plugins)
         @table << Mactag::Tag::Plugin.new(*plugins)
       end
       alias_method :plugins, :plugin
 
+      ##
+      #
       # @see Mactag::Tag::Gem
+      #
       def gem(*gems)
         @table << Mactag::Tag::Gem.new(*gems)
       end
       alias_method :gems, :gem
 
+      ##
+      #
       # @see Mactag::Tag::Rails
+      #
       def rails(options = {})
         @table << Mactag::Tag::Rails.new(options)
       end
-
     end
   end
 end
