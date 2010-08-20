d1aa5cf19175f8d999698618b877e5354e68a524
Johan Andersson <johan.rejeep@gmail.com>
2010-08-20 12:35:29 +0200

    Require some more classes.

--- lib/mactag.rb
+++ lib/mactag.rb
@@ -4,6 +4,9 @@ require 'mactag/builder'
 require 'mactag/dsl'
 require 'mactag/tag'
 require 'mactag/ctags'
+require 'mactag/server'
+require 'mactag/event_handler'
+require 'mactag/tags_file'
 
 module Mactag
   autoload :Bundler, 'bundler'
