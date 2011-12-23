812d129206c6b2d145c705d75ae11e80e2f96d8a
Johan Andersson <johan.rejeep@gmail.com>
2011-12-23 16:51:42 +0100

    Update config spec.

--- spec/mactag/config_spec.rb
+++ spec/mactag/config_spec.rb
@@ -1,16 +1,39 @@
 require 'spec_helper'
 
 describe Mactag::Config do
-  it 'should be configurable' do
-    Mactag.configure do |config|
-      config.rvm = false
-      config.binary = 'ctags -o {OUTPUT} -e {INPUT}'
-      config.tags_file = 'tags-foo'
-      config.gem_home = '/Library/Ruby/Gems/1.9/gems'
+  context 'configuration options' do
+    it "should have option 'binary'" do
+      Mactag::Config.should respond_to(:binary)
+      Mactag::Config.should respond_to(:binary=)
     end
 
-    Mactag::Config.binary.should == 'ctags -o {OUTPUT} -e {INPUT}'
-    Mactag::Config.tags_file.should == 'tags-foo'
-    Mactag::Config.rvm.should be_false
+    it "should have option 'tags_file'" do
+      Mactag::Config.should respond_to(:tags_file)
+      Mactag::Config.should respond_to(:tags_file=)
+    end
+
+    it "should have option 'rvm'" do
+      Mactag::Config.should respond_to(:rvm)
+      Mactag::Config.should respond_to(:rvm=)
+    end
+
+    it "should have option 'gem_home'" do
+      Mactag::Config.should respond_to(:gem_home)
+      Mactag::Config.should respond_to(:gem_home=)
+    end
+  end
+
+  it 'should be configurable' do
+    Mactag.configure do |config|
+      config.should == Mactag::Config
+    end
+  end
+
+  it "requires '{INPUT}' and '{OUTPUT}' in binary string" do
+    proc {
+      Mactag.configure do |config|
+        config.binary = 'ctags -o {foo} -e {bar}'
+      end
+    }.should raise_exception
   end
 end
